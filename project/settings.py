4cfa9567d861e57988dd5149a39b9888bb81c0f8
alexHooper <alexmohack@gmail.com>
2018-10-18 23:51:18 +0530

    configure jet dashboard

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django-jet
+    'jet',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
